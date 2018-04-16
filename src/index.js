ccace71a9a1d706c74da4749176e3032142bde30
tonykonst <akonst5@gmail.com>
2018-04-16 18:42:00 +0300

    added stage 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!'); //eslint-disable-line no-console
 
 const userName = readlineSync.question('May I have your name? ');
 
