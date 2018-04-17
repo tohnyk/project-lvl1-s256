9796e109790cb94df3a23418cb2c947b6a8bee10
tonykonst <akonst5@gmail.com>
2018-04-17 12:39:18 +0300

    fixed linter issues stage 3

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import userName from '..';
 
-console.log(`Hello, ${userName}!`); //eslint-disable-line no-console
+console.log(`Hello, ${userName}!`); // eslint-disable-line no-console
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!'); //eslint-disable-line no-console
+console.log('Welcome to the Brain Games!'); // eslint-disable-line no-console
 
-const userName = readlineSync.question('May I have your name? ');
+const userName = readlineSync.question('\nMay I have your name? ');
 
 export default userName;
