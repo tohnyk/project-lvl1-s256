a1c32ea5b0f23932362c9898bd243e13a28741cc
tonykonst <akonst5@gmail.com>
2018-04-22 13:40:38 +0300

    fix balance-game iter instead for

--- src/games/balance.js
+++ src/games/balance.js
@@ -4,47 +4,52 @@ import { randomInt, gameSession } from '..';
 const instruction = 'Balance the given number.';
 
 const isBalanced = (n) => {
-  let difference = 0;
-  for (let i = 0; i < String(n).length; i += 1) {
-    if (difference > 1) return false;
-    if (Number(String(n)[i] - String(n)[i + 1]) > 1
-    || Number(String(n)[i] - String(n)[i + 1]) < 0) {
-      return false;
+  const iter = (counter, acc) => {
+    if (acc > 1) return false;
+    if (Number(String(n)[counter] - String(n)[counter + 1]) > 1
+    || Number(String(n)[counter] - String(n)[counter + 1]) < 0) return false;
+
+    if (Number(String(n)[counter] - String(n)[counter + 1]) === 1) {
+      return iter(counter + 1, acc + 1);
     }
-    if (Number(String(n)[i] - String(n)[i + 1]) === 1) {
-      difference += 1;
+    return true;
+  };
+  return iter(0, 0);
+};
+
+const digitsSum = (n) => {
+  const iter = (counter, acc) => {
+    if (counter < String(n).length) {
+      return iter(counter + 1, acc + Number(String(n)[counter]));
     }
-  }
-  return true;
+    return acc;
+  };
+  return iter(0, 0);
 };
 
 const balance = (n) => {
-  let result = '';
-  let digitsSum = 0;
+  const sum = digitsSum(n);
 
-  for (let i = 0; i < String(n).length; i += 1) {
-    digitsSum += Number(String(n)[i]);
-  }
+  const iter = (counter, acc) => {
+    const minDigit = Math.floor(sum / String(n).length);
+    const maxDigit = minDigit + 1;
+    const maxDigitsQuantity = sum % String(n).length;
+    const minDigitsQuantity = String(n).length - maxDigitsQuantity;
 
-  const minDigit = Math.floor(digitsSum / String(n).length);
-  const maxDigit = minDigit + 1;
-  const maxDigitsQuantity = digitsSum % String(n).length;
-  const minDigitsQuantity = String(n).length - maxDigitsQuantity;
-
-  for (let i = 0; i < minDigitsQuantity; i += 1) {
-    result += String(minDigit);
-  }
-
-  for (let i = 0; i < maxDigitsQuantity; i += 1) {
-    result += String(maxDigit);
-  }
-
-  return result;
+    if (counter < minDigitsQuantity) {
+      return iter(counter + 1, acc + String(minDigit));
+    }
+    if (counter < String(n).length) {
+      return iter(counter + 1, acc + String(maxDigit));
+    }
+    return acc;
+  };
+  return iter(0, '');
 };
 
 
 const balanceGame = () => {
-  const gameQuestion = randomInt(1, 1000);
+  const gameQuestion = randomInt(1, 10000);
   const correctAnswer = isBalanced(gameQuestion) ? gameQuestion : balance(gameQuestion);
   const gamePair = cons(gameQuestion, correctAnswer);
   return gamePair;
